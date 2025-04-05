b233275cba5598e5c5dc3a03b4e245316825e206
Anmo9776 <112003314+Anmo9776@users.noreply.github.com>
2025-04-05 11:51:22 +0530

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Anmo9776/GrokHub/main/Source.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Anmo9776/GrokHub/refs/heads/main/Source.lua"))()
